bd2c395b7f38334f4afb244d82031cbf7ed043cc
viral mandaviya <140678543+Veer2127@users.noreply.github.com>
2023-09-22 10:41:14 +0530

    bank management system.py

--- bank management system.py
+++ bank management system.py
@@ -1,75 +1,116 @@
-class BankAccount:
-    def __init__(self, account_number, name, balance=0):
-        self.account_number = account_number
-        self.name = name
-        self.balance = balance
+# importing connection file
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-            print(f"Deposited ${amount}. New balance: ${self.balance}")
-        else:
-            print("Invalid deposit amount")
+import connection
 
-    def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self.balance}")
-        else:
-            print("Invalid withdrawal amount or insufficient funds")
+import pymysql
 
-    def get_balance(self):
-        print(f"Account Balance for {self.name}: ${self.balance}")
+mydb1=pymysql.connect(host="localhost",user="root",password="",database="python_crud1")
+mycursor=mydb1.cursor()
+
+status=True
+
+while status:
+
+    menu="""
+            Menu
+
+            1) Store data
+            2) View data
+            3) Update data
+            4) Search data
+            5) Delete data
+
+        """
+    
+    print(menu)
+
+    choice=int(input("Enter Your Choice:"))
+    if choice==1:
+        #to insert data
+        name=input("Enter your name:")
+        account_type=input("Enter your account type:")
+        account_number=int(input("Enter your account number:"))
+        password=int(input("Enter Your password:"))
+
+        query="insert into bank(name,account_type,account_number,password) values('%s','%s',%s,%s)"
+        args=(name,account_type,account_number,password)
+
+        mycursor.execute(query % args)
+
+        #to save changes
+        mydb1.commit()
+        print("Data Inserted Successfully")
+
+    elif choice==2:
+        #to fatch all data from table
+        query="select * from bank"
+
+        mycursor.execute(query)
+
+        # to fetch all data from query
+
+        data=mycursor.fetchall()
+        print(data)
+
+    elif choice==3:
+        #update data
+
+        account_number=int(input("Enter Your account number:"))
+        name=input("Enter your name:")
+        account_type=input("Enter your account type:")
+        password=int(input("Enter your password:"))
+
+        query="Update bank set name='%s', account_type='%s',password=%s where account_number=%s"
+        args=(name,account_type,password,account_number)
+
+        mycursor.execute(query % args)
+
+        mydb1.commit()
+        print("Data Updated Successfully")
+
+    elif choice==4:
+        # search data
+
+        account_number=int(input("Enter Your account number:"))
+
+        query="select * from bank where account_number=%s"
+
+        args=(account_number)
+
+        mycursor.execute(query % args)
+
+        #retrieve all data in row variable
+        row=mycursor.fetchone()
+
+        #name=0 account_type=1 account_number=2  password=4
+
+        displayname=row[0]
+        displayaccount_type=row[1]
+        displayaccount_number=row[2]
+        displaypassword=row[3]
+
+        print("Name= ",displayname)
+        print("account number= ",displayaccount_number)
+        print("account type= ",displayaccount_type)
+        print("password= ",displaypassword)
+
+    elif choice==5:
+        #delete data
+
+        account_number=int(input("Enter account number:"))
 
 
-def main():
-    accounts = {}
+        query="delete from bank where account_number=%s"
 
-    while True:
-        print("\nBanking Application Menu:")
-        print("1. Create Account")
-        print("2. Deposit")
-        print("3. Withdraw")
-        print("4. Check Balance")
-        print("5. Exit")
-        choice = input("Enter your choice: ")
+        args=(account_number)
 
-        if choice == "1":
-            account_number = input("Enter account number: ")
-            name = input("Enter account holder name: ")
-            accounts[account_number] = BankAccount(account_number, name)
-            print(f"Account created for {name} with account number {account_number}")
+        mycursor.execute(query % args)
 
-        elif choice == "2":
-            account_number = input("Enter account number: ")
-            amount = float(input("Enter deposit amount: "))
-            if account_number in accounts:
-                accounts[account_number].deposit(amount)
-            else:
-                print("Account not found")
+        mydb1.commit()
+        print("Data Deleted Successfully")
 
-        elif choice == "3":
-            account_number = input("Enter account number: ")
-            amount = float(input("Enter withdrawal amount: "))
-            if account_number in accounts:
-                accounts[account_number].withdraw(amount)
-            else:
-                print("Account not found")
+    loop_choice=input("Do You want to perform more operation? press 'y' for yes and 'n' for no:")
 
-        elif choice == "4":
-            account_number = input("Enter account number: ")
-            if account_number in accounts:
-                accounts[account_number].get_balance()
-            else:
-                print("Account not found")
-
-        elif choice == "5":
-            print("Exiting the program. Goodbye!")
-            break
-
-        else:
-            print("Invalid choice. Please select a valid option.")
-
-
-if __name__ == "__main__":
-    main()
+    if loop_choice=='n' or loop_choice=='no':
+        status=False    
+        print("Thank You For Visiting Our Bank!")
